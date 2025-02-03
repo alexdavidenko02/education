35913be4dd4bfd757bc8906e0a82663f04f03918
Alex <test@example.com>
2025-02-03 17:49:36 +0200

    049

--- js/script.js
+++ js/script.js
@@ -1,60 +1,19 @@
 'use strict'
 
-// function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-    
-//         return a + b;
-//     }
-
-//     console.log(sum());
-    
-    
-// }
-
-// showThis(4, 5);
-
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     sum: function () {
-//         function shout() {
-//             console.log(this);
-//         }
-
-//         shout();
-        
-//     }
-// }
-
-// obj.sum();
-
-// function User(name,id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function () {
-//         console.log(`Hello ${this.name}`);
-        
-//     }
-// }
-
-// let ivan = new User('Ivan', 23);
-
-function sayName() {
-    console.log(this);
-    console.log(this.name);
-}
-
-const user = {
-    name : 'john',
-};
-
-sayName.call(user);
-
-
-
 // 1) Обычная функция: this = window, но если стоит use-strict то будет undefined
 // 2) Контекст у методов объекта - сам объект
 // 3) this в конструкторах и классах это новый екземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
+
+
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+}
+
+log('basic', 'rest', 'operatot', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
