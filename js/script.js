0e5e43a6b0e176ccdc5449313764e33945ed3b50
Alex <test@example.com>
2025-02-13 20:06:29 +0200

    extra commit

--- js/script.js
+++ js/script.js
@@ -3,57 +3,44 @@
 // filter
 
 // const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
-
-// const shortNames = names.filter((name) => {
-//     return name.length < 5;
-// });
-
+// const shortNames = names.filter(name => name.length < 5);
 // console.log(shortNames);
-
 
 
 // map
 
-// let answers = ['IvAn', 'AnnA', 'Hello'];
-
-// answers = answers.map(item => item.toLowerCase());
-// console.log(answers);
-
+// const answers = ['IvAn', 'AnnA', 'Hello'];
+// const result = answers.map(item => item.toLowerCase());
+// console.log(result);
 
 
 // every/some
 
-// const some = [4, 'qwq', 'sdfsdfa'];
-
+// const some = [4, 'qwq', 'sdgfldfsgju'];
 // console.log(some.some(item => typeof(item) === 'number'));
 // console.log(some.every(item => typeof(item) === 'number'));
-
 
 
 // reduce
 
 // const arr = [4, 5, 1, 3, 2, 6];
+// const sum = arr.reduce((sum, item) => sum + item, 3);
+// console.log(sum);
 
-// const result = arr.reduce((sum, current) => sum + current);
+// const arr = ['test1', 'test2', 'test3'];
+// const newArr = arr.reduce((sum, item) => `${sum}, ${item}`);
+// console.log(newArr);
 
-// console.log(result);
-
-
-// const arr = ['apple', 'pear', 'plum'];
-
-// const result = arr.reduce((sum, current) => `${sum}, ${current}`, 3);
-// console.log(result);
 
 // const obj = {
 //     ivan: 'persone',
 //     ann: 'persone',
 //     dog: 'animal',
 //     cat: 'animal'
-// }
+// };
 
 // const newArr = Object.entries(obj)
 // .filter(item => item[1] === 'persone')
-// .map(item => item[0])
-// .reduce((sum ,item) => `${sum}, ${item}`);
+// .map(item => item[0]);
 
 // console.log(newArr);
